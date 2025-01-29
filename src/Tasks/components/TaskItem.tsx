d8edae57ade49838cf434f5b69353c4513d0618d
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-29 16:03:56 -0300

    Item de tarea

--- src/Tasks/components/TaskItem.tsx
+++ src/Tasks/components/TaskItem.tsx
@@ -1,9 +1,10 @@
 import Task from '@/models/Task';
+import { ListItem } from '@chakra-ui/react';
 
 type TaskItemProps = {
   task: Task;
 };
 
 export default function TaskItem({ task }: TaskItemProps) {
-  return <>{task.title}</>;
+  return <ListItem>{task.title}</ListItem>;
 }
