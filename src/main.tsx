03c45d9f1f6c457221d084d8cb8fba04b1cb5172
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-29 16:05:02 -0300

    Provider de react query agregado

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,20 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { Provider } from './components/ui/provider';
 import AuthProvider from './Auth/components/AuthProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>,
 );
