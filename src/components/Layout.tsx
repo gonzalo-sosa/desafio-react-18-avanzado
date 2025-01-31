13a2a74e74f70d665266944977112584055d2688
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-31 13:49:29 -0300

    Eliminado grid

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import NavBar from '@/components/NavBar';
 import SideBar from './SideBar';
-import { Grid } from '@chakra-ui/react';
 
 type Props = {
   children: ReactNode | ReactNode[];
@@ -11,10 +10,8 @@ export default function Layout({ children }: Props) {
   return (
     <>
       <NavBar />
-      <Grid templateColumns={'260px 1fr'} gap={2}>
-        <SideBar />
-        {children}
-      </Grid>
+      <SideBar />
+      {children}
     </>
   );
 }
