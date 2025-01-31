accf2fbd9370fdc69b89363aa5061ab141a948cd
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-31 10:51:03 -0300

    Contenido de barra lateral para espacio de trabajo

---
+++ src/components/WorkSpaceSideBar.tsx
@@ -0,0 +1,29 @@
+// type Props = {};
+
+import { Heading, HStack, IconButton, ListRoot } from '@chakra-ui/react';
+import { LuCalendar, LuChevronDown, LuTable } from 'react-icons/lu';
+import SideBarItem from './SideBarItem';
+
+export default function WorkSpaceSideBar() {
+  return (
+    <>
+      <HStack px={2} justifyContent={'space-between'}>
+        <Heading fontSize={'sm'} fontWeight={'normal'}>
+          Vistas del Espacio de trabajo
+        </Heading>
+        <IconButton variant={'outline'} size={'xs'}>
+          <LuChevronDown />
+        </IconButton>
+      </HStack>
+      <ListRoot listStyle={'none'}>
+        <SideBarItem navLinkProps={{ to: '/table' }}>
+          <LuTable />
+          Tabla
+        </SideBarItem>
+        <SideBarItem navLinkProps={{ to: '/calendar' }}>
+          <LuCalendar /> Calendario
+        </SideBarItem>
+      </ListRoot>
+    </>
+  );
+}
