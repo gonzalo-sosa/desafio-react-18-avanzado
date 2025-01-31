fdc838234bc02d96dcc847a0813dcc6afe70f564
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-31 15:55:01 -0300

    Añadidos componentes para login y registro

--- src/Auth/components/Login.tsx
+++ src/Auth/components/Login.tsx
@@ -1,27 +1,141 @@
-import { Box, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  Heading,
+  Image,
+  Input,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import useAuth from '../hooks/useAuth';
-import { useNavigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { LuTrello } from 'react-icons/lu';
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Field } from '@/components/ui/field';
 
-// type Props = {};
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+type FormData = z.infer<typeof schema>;
 
 export default function Login() {
   const { getUser, login } = useAuth();
   const navigate = useNavigate();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
-  const handleLogin = () => {
+  if (getUser()) {
+    return <Navigate to="/" />;
+  }
+
+  const handleLogin = ({ email, password }: FormData) => {
     login({
       id: String(Date.now()),
-      name: 'Gonzalo Sosa',
-      email: 'gonzalo@email.com',
+      name: email.substring(0, email.indexOf('@')),
+      email,
     });
     navigate('/', { state: { from: 'login' } });
   };
 
   return (
-    <Box padding={2}>
-      <Button onClick={() => handleLogin()} disabled={getUser() !== null}>
-        Login
-      </Button>
-    </Box>
+    <Grid placeItems={'center'}>
+      <Box display="flex" flexDirection="column" width="55%" maxWidth="500px">
+        <Box display="flex" flexDirection="column" alignItems="center" mb={3}>
+          <LuTrello size={100} />
+          <Heading textStyle={'md'}>Inicia sesión para continuar</Heading>
+        </Box>
+        <Flex
+          as={'form'}
+          onSubmit={handleSubmit(handleLogin)}
+          my={2}
+          mb={4}
+          flexDirection={'column'}
+        >
+          <Field
+            label="Email"
+            invalid={!!errors.email}
+            errorText={errors.email?.message}
+          >
+            <Controller
+              control={control}
+              defaultValue=""
+              name="email"
+              render={({ field }) => <Input {...field} />}
+            />
+          </Field>
+          <Field
+            label="Contraseña"
+            invalid={!!errors.password}
+            errorText={errors.password?.message}
+          >
+            <Controller
+              control={control}
+              defaultValue=""
+              name="password"
+              render={({ field }) => <Input {...field} />}
+            />
+          </Field>
+          <Button type="submit" my={4} disabled={!isValid}>
+            Iniciar sesión
+          </Button>
+        </Flex>
+        <Stack>
+          <Text textAlign={'center'} color={'gray.500'}>
+            O continúa con
+          </Text>
+          <Button w={'100%'} my={1} variant={'outline'}>
+            <Image
+              src="https://id-frontend.prod-east.frontend.public.atl-paas.net/assets/google-logo.5867462c.svg"
+              alt="Logo de Google"
+              htmlWidth={24}
+              marginLeft={2}
+            />
+            Google
+          </Button>
+          <Button w={'100%'} my={1} variant={'outline'}>
+            <Image
+              src="https://id-frontend.prod-east.frontend.public.atl-paas.net/assets/microsoft-logo.c73d8dca.svg"
+              alt="Logo de Microsoft"
+              htmlWidth={24}
+              marginLeft={2}
+            />
+            Microsoft
+          </Button>
+          <Button w={'100%'} my={1} variant={'outline'}>
+            <Image
+              src="https://id-frontend.prod-east.frontend.public.atl-paas.net/assets/apple-logo.54e0d711.svg"
+              alt="Logo de Apple"
+              htmlWidth={24}
+              marginLeft={2}
+            />
+            Apple
+          </Button>
+          <Button w={'100%'} my={1} variant={'outline'}>
+            <Image
+              src="https://id-frontend.prod-east.frontend.public.atl-paas.net/assets/slack-logo.5d730c10.svg"
+              alt="Logo de Slack"
+              htmlWidth={24}
+              marginLeft={2}
+            />
+            Slack
+          </Button>
+        </Stack>
+        <Box display="flex" flexDirection="column" alignItems="center">
+          <Link to="/register">¿No tiene una cuenta? Crea una</Link>
+          <br />
+        </Box>
+      </Box>
+    </Grid>
   );
 }
