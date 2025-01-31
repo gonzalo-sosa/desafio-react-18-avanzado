0145ca37f52f96551ac4ee3159ca396ba71f6be4
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-31 13:49:20 -0300

    Actualizado para modificar estado según se edita el título

--- src/Lists/components/ListCard.tsx
+++ src/Lists/components/ListCard.tsx
@@ -1,22 +1,28 @@
-// type Props = {};
-
 import List from '@/models/List';
+import useListsStore from '@/store/lists-store';
 import TasksList from '@/Tasks/components/TasksList';
 import {
   CardRoot,
   CardBody,
   CardHeader,
-  Heading,
   HStack,
   IconButton,
+  Editable,
+  useEditable,
 } from '@chakra-ui/react';
 import { LuDot } from 'react-icons/lu';
-
 type ListCardProps = {
   list: List;
 };
 
 export default function ListCard({ list }: ListCardProps) {
+  const [currentList] = useListsStore((s) => s.lists).filter(
+    (l) => l.id === list.id,
+  );
+  const editableTitle = useEditable({ defaultValue: list.title });
+
+  currentList.title = editableTitle.value;
+
   return (
     <CardRoot
       minW={'280px'}
@@ -27,9 +33,10 @@ export default function ListCard({ list }: ListCardProps) {
     >
       <CardHeader>
         <HStack justifyContent={'space-between'}>
-          <Heading fontSize={'sm'} fontWeight={'normal'}>
-            {list.title}
-          </Heading>
+          <Editable.RootProvider value={editableTitle}>
+            <Editable.Preview />
+            <Editable.Input />
+          </Editable.RootProvider>
           <IconButton variant={'outline'} size={'xs'}>
             <LuDot />
           </IconButton>
