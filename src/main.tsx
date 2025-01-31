b4d93cf0e9a3765792f1dd548c39be00a121de86
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-31 10:50:10 -0300

    Modificado nombre de provider para evitar confusión

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
-import { Provider } from './components/ui/provider';
+import { Provider as ChakraProvider } from './components/ui/provider';
 import AuthProvider from './Auth/components/AuthProvider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -11,13 +11,13 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
+    <ChakraProvider forcedTheme="light">
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <RouterProvider router={router} />
         </AuthProvider>
         <ReactQueryDevtools />
       </QueryClientProvider>
-    </Provider>
+    </ChakraProvider>
   </StrictMode>,
 );
