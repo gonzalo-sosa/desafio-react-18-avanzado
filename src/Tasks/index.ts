12814e6a8a960fb4740e24e8205631d1c3c0eb49
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-01-31 13:48:48 -0300

    Barrel file

---
+++ src/Tasks/index.ts
@@ -0,0 +1,3 @@
+export { default as TaskItem } from './components/TaskItem';
+export { default as TasksList } from './components/TasksList';
+export { default as AddTaskForm } from './components/AddTaskForm';
