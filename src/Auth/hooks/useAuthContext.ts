e808dfe3d844ccbb9ded77a6655f764d9030cfd1
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-02-09 23:52:08 -0300

    Cambio de nombre para explicitar que es un contexto

--- src/Auth/hooks/useAuthContext.ts
+++ src/Auth/hooks/useAuthContext.ts
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import AuthContext from '../context/AuthContext';
 
-const useAuth = () => useContext(AuthContext);
+const useAuthContext = () => useContext(AuthContext);
 
-export default useAuth;
+export default useAuthContext;
