21f56caabfae88b416a6cdf818017920024e82bb
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-02-10 00:06:12 -0300

    Solucionado problema de path

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import ProtectedRoutes from '@/components/ProtectedRoutes';
-import { Register, Login } from './Auth';
+import Register from '@/components/Register';
+import Login from '@/components/Login';
 import WorkSpacePage from './WorkSpace/components/WorkSpacePage';
 import BoardPage from './Boards/components/BoardPage';
 
