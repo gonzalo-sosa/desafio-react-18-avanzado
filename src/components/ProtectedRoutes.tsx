ece2f366242f167183d3cf202dc6e31baca99342
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-02-09 23:52:52 -0300

    Eliminado todo ya que está solucionado

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -5,7 +5,7 @@ import Layout from '@/components/Layout';
 export default function ProtectedRoutes() {
   const { getUser } = useAuth();
 
-  if (!getUser()) return <Navigate to="/login" />; // TODO: fix warning Cannot update a component (`AuthProvider`) while rendering a different component (`ProtectedRoutes`).
+  if (!getUser()) return <Navigate to="/login" />;
 
   return (
     <Layout>
