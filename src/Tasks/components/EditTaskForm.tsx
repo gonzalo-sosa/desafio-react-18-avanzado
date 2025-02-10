4f8a7162cb097f4ca5f8440efbf90a5454372278
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-02-10 11:08:13 -0300

    Formulario para editar una tarjeta

---
+++ src/Tasks/components/EditTaskForm.tsx
@@ -0,0 +1,50 @@
+import { Button, Fieldset, Input } from '@chakra-ui/react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm, Controller } from 'react-hook-form';
+import { z } from 'zod';
+import { Field } from '@/components/ui/field';
+
+const schema = z.object({
+  title: z
+    .string({ message: 'Título es obligatorio' })
+    .min(1, { message: '1 carácter como mínimo' }),
+});
+
+type FormData = z.infer<typeof schema>;
+
+interface EditTaskFormProps {
+  onSubmit: (data: FormData) => void;
+}
+
+export default function EditTaskForm({ onSubmit }: EditTaskFormProps) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Fieldset.Root>
+        <Fieldset.Legend>Editar tarjeta</Fieldset.Legend>
+        <Fieldset.Content>
+          <Field
+            label={'Título'}
+            invalid={!!errors.title}
+            errorText={errors.title?.message}
+          >
+            <Controller
+              name="title"
+              control={control}
+              defaultValue=""
+              render={({ field }) => <Input {...field} size={'xs'} autoFocus />}
+            />
+          </Field>
+        </Fieldset.Content>
+        <Button type="submit">Guardar</Button>
+      </Fieldset.Root>
+    </form>
+  );
+}
