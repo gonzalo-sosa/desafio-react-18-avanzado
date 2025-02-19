ee096fe056e6604db8a12625430afc72dd0b21ab
gonzalo <gonzalo.sosa@moxit.com.ar>
2025-02-19 14:48:10 -0300

    Eliminado react query

--- src/main.tsx
+++ src/main.tsx
@@ -2,22 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
-import { Provider as ChakraProvider } from './components/ui/provider';
-import { AuthProvider } from './Auth';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider forcedTheme="light">
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </ChakraProvider>
+    <RouterProvider router={router} />
   </StrictMode>,
 );
